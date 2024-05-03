b0bd4921c7b6e10f0e2b3d34a2782fd0bbcd48cb
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-05-03 21:58:57 +0300

    add

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,6 @@
 import {defineCliConfig} from 'sanity/cli'
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+export const projectId = "hekqjt0e";
+export const dataset = "production";
 
 export default defineCliConfig({
   api: {
